d1a537187084280a1d49cb1930bf53ccb513af69
Carlos Daniel Rivera <dr734659@gmail.com>
2025-03-13 09:07:45 -0500

    handler odifications

--- obj/Debug/net8.0/UsersAuthorization.AssemblyInfo.cs
+++ obj/Debug/net8.0/UsersAuthorization.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsersAuthorization")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bb112e71d7ebb2484926314d9b679c5aff012f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44cbf9f5db2a60963dee0d9a7389f7a98e6c6819")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsersAuthorization")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsersAuthorization")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
