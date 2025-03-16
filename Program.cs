7d0db9492c8d8aa231870e77d110fae2ba2e2faa
Daniel Rivera <daniel.riverar@pragma.com.co>
2025-03-16 15:04:44 -0500

    update swagger prod

--- Program.cs
+++ Program.cs
@@ -57,11 +57,11 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 // app.UseHttpsRedirection();
 app.UseAuthentication();
--- obj/Debug/net8.0/UsersAuthorization.AssemblyInfo.cs
+++ obj/Debug/net8.0/UsersAuthorization.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsersAuthorization")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6245b4c9d0624575562cf14c03857a1649c53fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f38c6543fe1beaf54dec5f624074e99304fefd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsersAuthorization")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsersAuthorization")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
